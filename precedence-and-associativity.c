9a6413b2dcdb9e247e450082a4e0fd9581c21306
Krisan Kumar Gupta <138972393+Krisan-2024-ME@users.noreply.github.com>
2023-07-13 15:14:14 +0530

    Update precedence-and-associativity.c

--- precedence-and-associativity.c
+++ precedence-and-associativity.c
@@ -15,5 +15,10 @@ int main(){
 	//precedence: {*,/,%}>{+,-}
 	//associativity: Left to right
 	int result= 5+5-2*7%2/2;
+	// => 3 + 1 - 35 % 3 / 2
+	// => 3 + 1 - 2 / 2
+	// => 3 + 1 - 1
+	// => 4 - 1
+	// => 3
 	printf("%d",result);
 }
