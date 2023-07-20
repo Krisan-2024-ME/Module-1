47dc4ab18d5df5f3042abb24f3c96ef3abb09560
Krisan Kumar Gupta <138972393+Krisan-2024-ME@users.noreply.github.com>
2023-07-20 20:09:21 +0530

    Add files via upload

---
+++ why 'char' categorized as integer.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<unistd.h>
+int main(){
+    //why 'char' is categorized as integer?
+    //Note: char , int and long are integer
+    char letter = '7';
+    //printf("%d",letter);
+    printf("%d",letter);
+    return 0;
+}
